8a2bee11fe331cf6aec408680a69b6ff96bf0a9f
David Neidhart <david.neidhart@gmx.net>
2025-12-25 03:07:31 +0100

    fix: AWS Rum not recording

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
 import { AppModule } from './app/app.module';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { initCwr } from './util/init-cwr';
+
+export const rum = initCwr();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch((err) => console.error(err));
---
+++ src/util/init-cwr.ts
@@ -0,0 +1,30 @@
+import { AwsRum, AwsRumConfig } from 'aws-rum-web';
+
+export function initCwr() {
+	try {
+		const config: AwsRumConfig = {
+			sessionSampleRate: 0.6 ,
+			identityPoolId: "eu-central-1:999ed878-9a34-44bb-849b-8dcd5fb4a733" ,
+			endpoint: "https://dataplane.rum.eu-central-1.amazonaws.com" ,
+			telemetries: ["performance","errors","http", "session"] ,
+			allowCookies: false ,
+			enableXRay: false ,
+			signing: true // If you have a public resource policy and wish to send unsigned requests please set this to false
+		};
+
+		const APPLICATION_ID: string = '8b62faa5-c08f-46c3-89a5-21ad3b3bc2d2';
+		const APPLICATION_VERSION: string = '1.0.0';
+		const APPLICATION_REGION: string = 'eu-central-1';
+
+		return new AwsRum(
+			APPLICATION_ID,
+			APPLICATION_VERSION,
+			APPLICATION_REGION,
+			config
+		);
+	} catch (error) {
+		// Ignore errors thrown during CloudWatch RUM web client initialization
+	}
+
+	return undefined;
+}
